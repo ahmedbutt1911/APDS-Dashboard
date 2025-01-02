34501b84e6d620a53920c3412479159189021463
Abdullah-hmed <abdullahwyi@gmail.com>
2025-01-02 22:15:56 +0500

    fitted tokenizer on cleaned text and saved as json to prevent repeated fitting + requirement.txt cross platform compatible

--- ServerSidePage/views.py
+++ ServerSidePage/views.py
@@ -7,26 +7,26 @@ import seaborn as sns
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+import os, json
 
-# import h5py
-# import re
-# import nltk
-# from nltk.stem import PorterStemmer 
-# from nltk.corpus import stopwords
-# from sklearn.model_selection import train_test_split
-# nltk.download('stopwords')
-# ps=PorterStemmer()
-# sw=stopwords.words('english')
+dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(dir, 'model_files/lstm_model.h5')
+tokenizer_dir = os.path.join(dir, 'model_files/tokenizer.json')
+# print(model_path)
+loaded_model = load_model(model_path)
 
+tokenizer = Tokenizer(num_words=100)
 
+with open(tokenizer_dir, 'r') as json_file:
+    tokenizer_json = json.load(json_file)
 
-loaded_model = load_model("D:/University/FYP/new_app_Django/APDS/DeepLearningModel/lstm_model.h5")
+tokenizer = tokenizer_from_json(tokenizer_json)
 
 
 # Create your views here.
 
 def preprocessing(Email_content):
-    tokenizer = Tokenizer(num_words=100)
     test_sequences = tokenizer.texts_to_sequences([Email_content])
     a=pad_sequences(test_sequences,padding='post',maxlen=100)
     return a
@@ -39,5 +39,5 @@ def prediction(request):
             processed_content = preprocessing(mail_content)
             print(processed_content)
             prediction_result = loaded_model.predict(processed_content)
-            result = "spam" if prediction_result[0] > 0.5 else "Legitimate Email"
+            result = "Spam Email" if prediction_result[0] > 0.5 else "Legitimate Email"
     return render(request,"Input.html", {'result':result})
