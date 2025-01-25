f235b81a5be735ee8f3b78e40557d68908160c70
Muhammad Ammad <me.ammad1786@gmail.com>
2025-01-25 19:07:14 +0500

    I have deleted server folder from this project, now it contains only the frontend

--- server/ServerSidePage/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User_loginCred
-# Register your models here.
-
-@admin.register(User_loginCred)
-class Useradmin(admin.ModelAdmin):
-    list_display = ['id','email','name']
--- server/ServerSidePage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import prediction, Home, logout_view
-
-urlpatterns = [
-    path("", Home, name="Home"),
-    path("logout_view", logout_view, name="logout_view"),
-    path("prediction", prediction, name="prediction"),
-    
-]
