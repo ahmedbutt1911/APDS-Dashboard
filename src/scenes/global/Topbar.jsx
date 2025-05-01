c96ffff647cf44c603c87a6a697b527e9a31d4a0
Muhammad Ammad <me.ammad1786@gmail.com>
2025-05-01 15:40:50 +0500

    Removed buttons from topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,17 +1,17 @@
-import { Box, IconButton, useTheme, InputBase } from '@mui/material';
-import { useContext } from 'react';
-import { ColorModeContext, tokens } from '../../theme';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import LogoutIcon from '@mui/icons-material/Logout';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Box, IconButton, useTheme, InputBase } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { googleLogout } from "@react-oauth/google";
-import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '../../stores/AuthStore';
-import { logout as authServiceLogout } from '../../services/authService'; // Import logout from authService
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../stores/AuthStore";
+import { logout as authServiceLogout } from "../../services/authService"; // Import logout from authService
 
 const Topbar = () => {
   const theme = useTheme();
@@ -27,7 +27,11 @@ const Topbar = () => {
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
       {/* Search Bar */}
-      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
         <InputBase placeholder="Search..." sx={{ ml: 2, flex: 1 }} />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchOutlinedIcon />
@@ -37,10 +41,14 @@ const Topbar = () => {
       {/* Icons */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === 'dark' ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />}
+          {theme.palette.mode === "dark" ? (
+            <LightModeOutlinedIcon />
+          ) : (
+            <DarkModeOutlinedIcon />
+          )}
         </IconButton>
 
-        <IconButton>
+        {/* <IconButton>
           <NotificationsOutlinedIcon />
         </IconButton>
 
@@ -50,7 +58,7 @@ const Topbar = () => {
 
         <IconButton>
           <SettingsOutlinedIcon />
-        </IconButton>
+        </IconButton> */}
 
         <IconButton onClick={handleLogout}>
           <LogoutIcon />
