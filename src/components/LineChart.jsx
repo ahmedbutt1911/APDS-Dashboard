8ea966dd791c7b2d14a8bbbd76a1c89942c3545a
Muhammad Ammad <me.ammad1786@gmail.com>
2025-05-01 15:27:31 +0500

    Dashboard API Finalized

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -3,13 +3,13 @@ import { useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import { mockLineData as data } from "../data/mockData";
 
-const LineChart = ({ isDashboard = false }) => {
+const LineChart = ({ isDashboard = false, lineData }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
     <ResponsiveLine
-      data={data}
+      data={lineData}
       theme={{
         axis: {
           domain: {
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -14,18 +14,46 @@ import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 import ReportIcon from "@mui/icons-material/Report";
+import { useEffect, useState } from "react";
+import { useAuthStore } from "../../stores/AuthStore";
+import axios from "axios";
+import config from "../../config";
 
 const Dashboard = () => {
+  const { user } = useAuthStore();
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${config.backendUrl}/dashboard`, {
+          headers: {
+            Authorization: `Bearer ${user?.access_token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching email data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <Box m="20px">
-      {/* HEADER */}
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="Dashboard" subtitle="Data Insights are provided below" />
+    data && (
+      <Box m="20px">
+        {/* HEADER */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header
+            title="Dashboard"
+            subtitle="Data Insights are provided below"
+          />
 
-        {/* <Box>
+          {/* <Box>
           <Button
             sx={{
               backgroundColor: colors.blueAccent[700],
@@ -39,194 +67,205 @@ const Dashboard = () => {
             Download Reports
           </Button>
         </Box> */}
-      </Box>
-
-      {/* GRID & CHARTS */}
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows="140px"
-        gap="20px"
-      >
-        {/* ROW 1 */}
-        <Box
-          gridColumn="span 4"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="12,361"
-            subtitle="Total Emails Detected"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 4"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="431,225"
-            subtitle="Valid Emails"
-            progress="0.50"
-            increase="+21%"
-            icon={
-              <VerifiedUserIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 4"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="32,441"
-            subtitle="Detected Spam Emails"
-            progress="0.30"
-            increase="+5%"
-            icon={
-              <ReportIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
         </Box>
 
-        {/* ROW 2 */}
+        {/* GRID & CHARTS */}
         <Box
-          gridColumn="span 8"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gridAutoRows="140px"
+          gap="20px"
         >
-          <Typography variant="h5" fontWeight="600">
-            Total Spam Detected
-          </Typography>
+          {/* ROW 1 */}
           <Box
+            gridColumn="span 4"
+            backgroundColor={colors.primary[400]}
             display="flex"
-            flexDirection="column"
             alignItems="center"
-            mt="25px"
+            justifyContent="center"
           >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              25% Emails detected to be SPAM
-            </Typography>
-            <Typography>Errors and omissions are accepted</Typography>
+            <StatBox
+              title={data?.total_emails}
+              subtitle="Total Emails Detected"
+              icon={
+                <EmailIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                />
+              }
+            />
           </Box>
-        </Box>
-
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          overflow="auto"
-        >
           <Box
+            gridColumn="span 4"
+            backgroundColor={colors.primary[400]}
             display="flex"
-            justifyContent="space-between"
             alignItems="center"
-            borderBottom={`4px solid ${colors.primary[500]}`}
-            colors={colors.grey[100]}
-            p="15px"
+            justifyContent="center"
           >
-            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
-              Recent Analysis
-            </Typography>
+            <StatBox
+              title={data?.category_summary?.Legitimate?.count}
+              subtitle="Valid Emails"
+              progress={data?.category_summary?.Legitimate?.percentage / 100}
+              increase={`${data?.category_summary?.Legitimate?.percentage}%`}
+              icon={
+                <VerifiedUserIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                />
+              }
+            />
           </Box>
-          {mockEmails.map((email, i) => (
+          <Box
+            gridColumn="span 4"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <StatBox
+              title={data?.category_summary?.Spam?.count}
+              subtitle="Detected Spam Emails"
+              progress={data?.category_summary?.Legitimate?.percentage / 100}
+              increase={`${data?.category_summary?.Spam?.percentage}%`}
+              icon={
+                <ReportIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                />
+              }
+            />
+          </Box>
+
+          {/* ROW 2 */}
+          <Box
+            gridColumn="span 4"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            p="30px"
+          >
+            <Typography variant="h5" fontWeight="600">
+              Total Spam Detected
+            </Typography>
             <Box
-              key={`${email.emailId}-${i}`}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              mt="25px"
+            >
+              <ProgressCircle
+                size="125"
+                progress={data?.category_summary?.Legitimate?.percentage / 100}
+              />
+              <Typography
+                variant="h5"
+                color={colors.greenAccent[500]}
+                sx={{ mt: "15px" }}
+              >
+                {`${data?.category_summary?.Spam?.percentage}% Emails detected to be SPAM`}
+              </Typography>
+              <Typography>Errors and omissions are accepted</Typography>
+            </Box>
+          </Box>
+
+          <Box
+            gridColumn="span 8"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            overflow="auto"
+          >
+            <Box
               display="flex"
               justifyContent="space-between"
               alignItems="center"
               borderBottom={`4px solid ${colors.primary[500]}`}
+              colors={colors.grey[100]}
               p="15px"
+            >
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+              >
+                Recent Analysis
+              </Typography>
+            </Box>
+            {data.recent_emails.map((email, i) => (
+              <Box
+                key={`${i}`}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                borderBottom={`4px solid ${colors.primary[500]}`}
+                p="15px"
+              >
+                <Box>
+                  <Typography
+                    color={colors.greenAccent[500]}
+                    variant="h5"
+                    fontWeight="600"
+                  >
+                    {email.title}
+                  </Typography>
+                  <Typography color={colors.grey[100]}>{email.body}</Typography>
+                </Box>
+                <Box color={colors.grey[100]} width="fit-content">
+                  {email.date}
+                </Box>
+                <Box
+                  backgroundColor={
+                    email.status === "Legitimate"
+                      ? colors.greenAccent[500]
+                      : colors.redAccent[500]
+                  }
+                  p="5px 10px"
+                  borderRadius="4px"
+                >
+                  {email.status}
+                </Box>
+              </Box>
+            ))}
+          </Box>
+
+          {/* ROW 3 */}
+          <Box
+            gridColumn="span 12"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+          >
+            <Box
+              mt="25px"
+              p="0 30px"
+              display="flex "
+              justifyContent="space-between"
+              alignItems="center"
             >
               <Box>
                 <Typography
-                  color={colors.greenAccent[500]}
                   variant="h5"
                   fontWeight="600"
+                  color={colors.grey[100]}
                 >
-                  {email.emailId}
+                  Phishing Attacks are Increasing
                 </Typography>
-                <Typography color={colors.grey[100]}>{email.from}</Typography>
+                <Typography
+                  variant="h3"
+                  fontWeight="bold"
+                  color={colors.greenAccent[500]}
+                >
+                  Trends
+                </Typography>
               </Box>
-              <Box color={colors.grey[100]}>{email.date}</Box>
-              <Box
-                backgroundColor={
-                  email.safe ? colors.greenAccent[500] : colors.redAccent[500]
-                }
-                p="5px 10px"
-                borderRadius="4px"
-              >
-                {email.safe ? `Valid` : `Spam`}
+              <Box>
+                <IconButton>
+                  <DownloadOutlinedIcon
+                    sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                  />
+                </IconButton>
               </Box>
             </Box>
-          ))}
-        </Box>
-
-        {/* ROW 3 */}
-        <Box
-          gridColumn="span 12"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-        >
-          <Box
-            mt="25px"
-            p="0 30px"
-            display="flex "
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              <Typography
-                variant="h5"
-                fontWeight="600"
-                color={colors.grey[100]}
-              >
-                Phishing Attacks are Increasing
-              </Typography>
-              <Typography
-                variant="h3"
-                fontWeight="bold"
-                color={colors.greenAccent[500]}
-              >
-                Trends
-              </Typography>
-            </Box>
-            <Box>
-              <IconButton>
-                <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
-                />
-              </IconButton>
+            <Box height="250px" m="-20px 0 0 0">
+              <LineChart isDashboard={true} lineData={data?.line_chart} />
             </Box>
           </Box>
-          <Box height="250px" m="-20px 0 0 0">
-            <LineChart isDashboard={true} />
-          </Box>
-        </Box>
 
-        {/* <Box
+          {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -242,7 +281,7 @@ const Dashboard = () => {
             <BarChart isDashboard={true} />
           </Box>
         </Box> */}
-        {/* <Box
+          {/* <Box
           gridColumn="span 6"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -259,8 +298,9 @@ const Dashboard = () => {
             <GeographyChart isDashboard={true} />
           </Box>
         </Box> */}
+        </Box>
       </Box>
-    </Box>
+    )
   );
 };
 
